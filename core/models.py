70523a04efa6a366195bf6bff6567a9ed9121c9e
personal <suhaillahmadd0@gmail.com>
2024-03-27 19:06:48 +0530

    changed id to uuid field

--- core/models.py
+++ core/models.py
@@ -1,7 +1,10 @@
+import uuid
+
 from django.db import models
 
 
 class Book(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     authors = models.CharField(max_length=200)
     publication_date = models.DateField(null=True, blank=True)
