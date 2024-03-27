995326016c96912f41b3884d903d7a30dcf3f942
personal <suhaillahmadd0@gmail.com>
2024-03-27 13:10:10 +0530

    initialized project and added book model

---
+++ core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    authors = models.CharField(max_length=200)
+    publication_date = models.DateField(null=True, blank=True)
+    ISBN = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return f"{self.title}"
